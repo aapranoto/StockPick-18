b33dd41a82f22b1dbb117586d146d375fdc0b1fc
aapranoto <43553843+aapranoto@users.noreply.github.com>
2018-10-13 22:10:56 -0700

    Add files via upload

---
+++ StockSelect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockPick_18
+{
+    /// <summary>
+    /// Select updbeat traded stock
+    /// 
+    /// </summary>
+
+    class StockSelect
+    {
+        /// <summary>
+        /// Key accounted parameters
+        /// </summary>
+        public string StName { get; }
+        public decimal StPriceChg { get;  }
+        public decimal StFreqChg { get;  }
+        public decimal StTrdAmtChg { get;  }
+        public string StTrend { get;  }
+        public DateTime PointTime { get;  }
+    }
+}
